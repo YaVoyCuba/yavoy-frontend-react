2ffc517499f75527183264292f5c1a118dbdceab
Royler Marichal <cluzstudio@gmail.com>
2022-11-18 16:57:25 -0500

    no resturants

--- src/Components/Landing/Restaurants.jsx
+++ src/Components/Landing/Restaurants.jsx
@@ -102,6 +102,19 @@ const Restaurants = () => {
                 </div>
               );
             })}
+
+            {restaurants.length == 0 && (
+              <div className="col-span-3 my-2 lg:col-span-1">
+                <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+                  <div className="px-4 py-5 sm:px-6">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900">
+                      No hay restaurantes disponibles en esta zona
+                    </h3>
+                    </div>
+                    </div>
+                    </div>
+            )}
+
           </div>
  
         </div>

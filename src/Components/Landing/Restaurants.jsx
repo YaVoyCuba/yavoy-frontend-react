553aa7d58cc808d4d44d9c405980d71bc6c2e386
Camilo <kmilo.denis.glez@yandex.com>
2023-09-18 09:43:19 -0500

    replace "No hay restaurantes..." by "No hay servicios..."

--- src/Components/Landing/Restaurants.jsx
+++ src/Components/Landing/Restaurants.jsx
@@ -293,7 +293,7 @@ const Restaurants = () => {
                 <div className="bg-white shadow overflow-hidden sm:rounded-lg">
                   <div className="px-4 py-5 sm:px-6">
                     <h3 className="text-lg leading-6 font-medium text-gray-900">
-                      No hay restaurantes disponibles en esta zona
+                      No hay servicios disponibles en esta zona
                     </h3>
                   </div>
                 </div>

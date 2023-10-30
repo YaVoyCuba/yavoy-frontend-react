3473f14e388b43dc97ca1f845a5b3884fa2965ed
Camilo <kmilo.denis.glez@yandex.com>
2023-10-30 10:42:19 -0500

    replace svg flag icon by point

--- src/Components/Header/Location.jsx
+++ src/Components/Header/Location.jsx
@@ -135,19 +135,13 @@ const Location = () => {
                         getLocation();
                     } }
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={ 1.5 }
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"
-                        />
+                    <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                         viewBox="0 0 17 21">
+                        <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                            <path d="M8 12a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                            <path
+                                d="M13.8 12.938h-.01a7 7 0 1 0-11.465.144h-.016l.141.17c.1.128.2.252.3.372L8 20l5.13-6.248c.193-.209.373-.429.54-.66l.13-.154Z" />
+                        </g>
                     </svg>
                     <span className="text-gray-700 text-sm font-medium pl-3">
             Delivery fijado en { location.locationName }

7b865e975597f405a5c7ebbdee65c4e31b040a6d
Camilo <kmilo.denis.glez@yandex.com>
2023-10-30 10:13:11 -0500

    add province and municipality location state

--- src/redux/locationSlice.js
+++ src/redux/locationSlice.js
@@ -5,9 +5,17 @@ const locationSlice = createSlice( {
     name:         'location',
     initialState: {
         location:      { locationName: '', locationId: 0, provinceName: '', provinceId: 0 },
+        province:      { label: '', value: null },
+        municipality: { label: '', value: null },
         locationHouse: { 'locationName': '', 'locationId': 0, 'provinceId': 0 },
     },
     reducers:     {
+        setProvince: ( state, action ) => {
+            state.province = action.payload;
+        },
+        setMunicipality: ( state, action ) => {
+            state.municipality = action.payload;
+        },
         setLocationCommerce: ( state, action ) => {
             state.location = action.payload;
         },

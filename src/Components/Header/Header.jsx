831fde45ad2a2975213e0d4265225573ddfe1b34
Camilo <kmilo.denis.glez@yandex.com>
2023-11-01 19:27:26 -0500

    fix Header issue

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = ( props ) => {
     const locationType = props.locationType;
 
     return (
-        <div class="header-fixed-top">
+        <div className="header-fixed-top">
             <HeaderTop />
             <Location />
         </div>

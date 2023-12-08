78577aa410ace5096ff867412f944465f2dc16ef
Camilo <kmilo.denis.glez@yandex.com>
2023-12-08 16:58:42 -0500

    clean App.jsx code

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,83 @@
-
-import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ProductDetailPage from "./Pages/frontend/ProductDetailPage";
-import Restaurants from "./Components/Landing/Restaurants";
-import TemplateLanding from "./Components/Templates/TemplateLanding";
-import RestaurantPage from "./Pages/frontend/RestaurantPage";
-import { ToastContainer } from "react-toastify";
-import CheckOut from "./Pages/frontend/CheckOut";
-import PaymentCompleted from "./Pages/frontend/PaymentCompleted";
-import PaymentFailed from "./Pages/frontend/PaymentFailed";
-import { persistor, store } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ProductDetailPage from './Pages/frontend/ProductDetailPage';
+import Restaurants from './Components/Landing/Restaurants';
+import TemplateLanding from './Components/Templates/TemplateLanding';
+import RestaurantPage from './Pages/frontend/RestaurantPage';
+import { ToastContainer } from 'react-toastify';
+import CheckOut from './Pages/frontend/CheckOut';
+import PaymentCompleted from './Pages/frontend/PaymentCompleted';
+import PaymentFailed from './Pages/frontend/PaymentFailed';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 function App() {
-  // initialize a browser router
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <TemplateLanding />,
-      // loader: rootLoader,
-      children: [
+    // initialize a browser router
+    const router = createBrowserRouter( [
         {
-          path: "/",
-          element: <Restaurants />,
+            path:    '/',
+            element: <TemplateLanding />,
+            // loader: rootLoader,
+            children: [
+                {
+                    path:    '/',
+                    element: <Restaurants />,
+                },
+                {
+                    path:    '/mercados',
+                    element: <Restaurants />,
+                },
+                {
+                    path:    '/servicios',
+                    element: <Restaurants />,
+                },
+                {
+                    path:    '/restaurants',
+                    element: <Restaurants />,
+                },
+                {
+                    path:    '/regalitos',
+                    element: <Restaurants />,
+                },
+                {
+                    path:    '/dulcerias',
+                    element: <Restaurants />,
+                },
+                {
+                    path:    '/restaurante/:restaurantSlug',
+                    element: <RestaurantPage />,
+                },
+                {
+                    path:    '/pagocompletado',
+                    element: <PaymentCompleted />,
+                },
+                {
+                    path:    '/errorenpago',
+                    element: <PaymentFailed />,
+                },
+                {
+                    path:    '/producto/:productSlug',
+                    element: <ProductDetailPage />,
+                },
+                {
+                    path:    '/caja',
+                    element: <CheckOut />,
+                },
+            ],
         },
-        {
-          path: "/mercados",
-          element: <Restaurants />,
-        },
-        {
-          path: "/servicios",
-          element: <Restaurants />,
-        },
-        {
-          path: "/restaurants",
-          element: <Restaurants />,
-        },
-        {
-          path: "/regalitos",
-          element: <Restaurants />,
-        },
-        {
-          path: "/dulcerias",
-          element: <Restaurants />,
-        },
-        {
-          path: "/restaurante/:restaurantSlug",
-          element: <RestaurantPage />,
-        },
-        {
-          path: "/pagocompletado",
-          element: <PaymentCompleted />,
-        },
-        {
-          path: "/errorenpago",
-          element: <PaymentFailed />,
-        },
-        {
-          path: "/producto/:productSlug",
-          element: <ProductDetailPage />,
-        },
-        {
-          path: "/caja",
-          element: <CheckOut />,
-        },
-      ],
-    },
-  ])
+    ] );
 
-
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-          <div className="relative">
-            <RouterProvider router={router} />
-          </div>
-          <ToastContainer />
-      </PersistGate>
-    </Provider>
-  );
+    return (
+        <Provider store={ store }>
+            <PersistGate loading={ null } persistor={ persistor }>
+                <div className="relative">
+                    <RouterProvider router={ router } />
+                </div>
+                <ToastContainer />
+            </PersistGate>
+        </Provider>
+    );
 }
 
 export default App;

686d7abec3aa1ffd4a3d20ac63c58ffa439b3d3c
Camilo <kmilo.denis.glez@yandex.com>
2023-12-11 16:28:55 -0500

    add queryParamHook

---
+++ src/utils/queryParamHook.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useSearchParams } from 'react-router-dom';
+import * as JSURL from "jsurl";
+import { DEFAULT_LOCATION_DATA } from './constants.js';
+
+/**
+ * This custom hook is a wrapper around `useSearchParams()` that parses and
+ * serializes the search param value using the JSURL library, which permits any
+ * JavaScript value to be safely URL-encoded.
+ *
+ */
+const useQueryParam = (key) => {
+    let [searchParams, setSearchParams] = useSearchParams();
+    let paramValueSTR = searchParams.get(key);
+
+    if (!paramValueSTR) {
+        paramValueSTR = JSURL.stringify(DEFAULT_LOCATION_DATA);
+    }
+
+    let value = React.useMemo(() => JSURL.parse(paramValueSTR), [paramValueSTR]);
+    let setValue = React.useCallback(
+        (newValue, options) => {
+            let newSearchParams = new URLSearchParams(searchParams);
+            newSearchParams.set(key, JSURL.stringify(newValue));
+            setSearchParams(newSearchParams, options);
+        },
+        [key, searchParams, setSearchParams]
+    );
+
+    return [value, setValue];
+};
+
+export default useQueryParam;
